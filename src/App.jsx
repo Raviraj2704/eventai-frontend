1f03bd568ababa1f3c14020676fe947ab9b5e6b0
raviraj2704 <ravirajapanthulu@gmail.com>
2026-09-14 18:04:40 +0530

    Fix AdminDashboardScreen capital file path

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ import LearningPathsScreen from './pages/gamification/LearningPathsScreen'
 
 // Pages - Engagement Center & Admin
 import EngagementCenterScreen from './pages/engagement-center/engagementCenterScreen.jsx'
-import adminDashboardScreen from './pages/admin/adminDashboardScreen.jsx'
+import AdminDashboardScreen from './pages/admin/AdminDashboardScreen.jsx'
 
 // Components
 import PrivateRoute from './components/Auth/PrivateRoute'
