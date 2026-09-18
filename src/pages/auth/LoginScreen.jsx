651989ffd185f70d71bd22189fe1b8880973802f
raviraj2704 <ravirajapanthulu@gmail.com>
2026-09-18 15:21:20 +0530

    Fix store folder path typo in LoginScreen

--- src/pages/auth/LoginScreen.jsx
+++ src/pages/auth/LoginScreen.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuthStore, useAuthLoading, useAuthError } from '@/stores/authStore';
+import { useAuthStore, useAuthLoading, useAuthError } from '../../store/authStore';
 import styles from '../../styles/LoginPage.module.css';
 
 /**
