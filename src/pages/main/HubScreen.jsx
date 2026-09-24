92d8e778394e128cfebea05a825750320986af3c
Raviraj2704 <ravirajapanthulu@gmail.com>
2026-09-24 15:28:59 +0530

    Update HubScreen.jsx

--- src/pages/main/HubScreen.jsx
+++ src/pages/main/HubScreen.jsx
@@ -7,7 +7,7 @@
 
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { apiGet } from '../services/api';
+import { apiGet } from '../../services/api';
 import '../../styles/hub.css';
 
 export default function HubPage() {
